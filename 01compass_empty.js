f173fbb03aab79abb59ee87532e5be35087af964
fffffffffei <apple@feideMacBook-Pro.local>
2018-02-18 23:25:12 -0600

    Initial

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -8,7 +8,18 @@ oriented = function(e) {
             compassHeading = e.alpha;
         }
         
-         text_area.textContent = compassHeading;
+        if(compassHeading >315 || compassHeading < 45){
+            text_area.textContent = "North, a pink pig";
+        }
+        if(compassHeading >45 && compassHeading < 135){
+            text_area.textContent = "East, a yellow pig";
+        }
+        if(compassHeading >135 && compassHeading < 225){
+            text_area.textContent = "South, a blue pig";
+        }
+        if(compassHeading >225 && compassHeading < 315){
+            text_area.textContent = "West, a red pig";
+        }
         //calls function to alter content based on heading
        // myOrientation();
     }
